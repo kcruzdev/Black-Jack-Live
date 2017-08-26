067d3a5513d407b95ab8d53d9f9f869b5ae0adbf
ConditionsChange <kevcruz7@aol.com>
2017-08-26 09:11:49 -0700

    presentation

--- controllers/blackjack_controller.js
+++ controllers/blackjack_controller.js
@@ -21,32 +21,90 @@ module.exports = function(app){
         where: {name: name, password: password},
       }).then(function(dbblackjack){
         if (dbblackjack === null){
-          res.render("login");
+          res.json("Error wrong login info")
         }else{
-          res.render("game")
+          res.redirect("/game/:" + name)
         }
       })
       });
 
     //POST route for creating a new account
     app.post("/", function(req, res) {
-      console.log(req.body);
-      db.blackjack.create({
-        name: req.body.name,
-        password: req.body.password,
-        bank: 500 
-      }).then(function(dbblackjack) {
-        res.redirect("/game");
-      });
-    });
+      name = req.body.name
+      password: req.body.password
+
+      db.blackjack.findOne({
+        where: {name:  name},
+      }).then(function(dbblackjack){
+        if (dbblackjack === null){
+
+          console.log(req.body);
+          db.blackjack.create({
+            name: req.body.name,
+            password: req.body.password,
+            bank: 1000.00 
+          }).then(function(dbblackjack) {
+            res.redirect("/game/" + name);
+          });
+
+
+
+        }else{
+          res.json("Error username already exists")
+        }
+      })
+    })
+
+
+
+
 
 //////////////////////////////////////////////////////////////////////////////
 
 
      //GET route to show the game
-     app.get("/game", function(req,res){
-      res.render("game");
+     app.get("/game/:player", function(req,res){
+
+      playerId = req.params.player; //make sure the form name has the id "player"
+      db.blackjack.findOne({
+        where: {name:  playerId},
+      }).then(function(dbblackjack){
+        if (dbblackjack === null){
+          res.json("Error 404 user does not exist")
+        }else{
+          res.render("game");
+        }
+      })
        });
+
+       app.put("/game/update/:player/:bank", function(req,res){
+        console.log("update request recieved")
+        name = req.params.player;
+        bank = req.params.bank;
+        console.log(bank)
+        bank = bank.replace( /,/g, "" );
+        bank = parseFloat(bank)
+        db.blackjack.update({
+          bank:bank
+        }, {
+          where: {
+            name: name
+          }
+        }).then(function(dbTodo) {
+          // res.redirect("/");
+        });
+      });
+
+
+      app.put("/game/getbank/:player", function(req,res){
+        name = req.params.player;
+        db.blackjack.findOne({
+          where: {name:  playerId},
+        }).then(function(dbblackjack){
+          console.log(dbblackjack.bank)
+          res.json({money:dbblackjack.bank})
+        })         
+      })
 
 //////////////////////////////////////////////////////////////////////////////
 
--- models/blackjack.js
+++ models/blackjack.js
@@ -2,7 +2,7 @@ module.exports = function(sequelize, DataTypes){
     var blackjack = sequelize.define("blackjack",{
         name: DataTypes.STRING,
         password: DataTypes.STRING,
-        bank: {type: DataTypes.FLOAT, defaultValue:500}
+        bank: {type: DataTypes.FLOAT, defaultValue:1000.00}
     });
     return blackjack
 }
